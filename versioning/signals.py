3bb15d923f59887fb3d5a9be252b0206fa8cc71e
EvoTech <iv_zak@yahoo.com>
2012-02-14 09:02:11 +0200

    treadedlocal for request data as default editor info

--- versioning/signals.py
+++ versioning/signals.py
@@ -1,3 +1,4 @@
+from .middleware import get_request
 from .models import Revision
 from .utils import obj_diff
 
@@ -9,6 +10,14 @@ def pre_save(instance, **kwargs):
     original = model._default_manager.get(pk=instance.pk)
     delta = obj_diff(instance, original)
     info = getattr(instance, 'revision_info', {})
+    request = get_request()
+    if request:
+        if not info.get('editor'):
+            info['editor'] = request.user
+        if not info.get('editor_ip'):
+            info['editor_ip'] = request.META.get("REMOTE_ADDR")
+    if not getattr(info['editor'], 'pk'):
+        info['editor'] = None
     rev = Revision(delta=delta, **info)
     rev.content_object = instance
     rev.save()
